9779dd345abe7d32aad89fba3ece2216cd23d82f
Nicolas <nicolascari.rz@gmail.com>
2022-08-23 00:01:17 -0400

    hasta 7% de descuento

--- src/sumador.js
+++ src/sumador.js
@@ -8,13 +8,17 @@ function multiplicar(a, b) {
 
 function descuento(tot) {
   let des = 0;
-  if (tot >= 1000 && tot <=3000)
+  if (tot >= 1000 && tot <3000)
   {
     des = 0.03;
   }
-  else if (tot >= 3000 && tot <= 7000)
+  else if (tot >= 3000 && tot < 7000)
   {
     des = 0.05;
+  }
+  else if (tot >= 7000 && tot < 10000)
+  {
+    des = 0.07;
   }
   return des;
 }
